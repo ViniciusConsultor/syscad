acc72d79bcdb8576cd95643ca6953a20145825fa
leolirarj <leolirarj@gmail.com>
2012-04-14 19:56:21 +0000

    reestruturação do projeto

--- Persistence/DAO/Repositorio.cs
+++ Persistence/DAO/Repositorio.cs
@@ -4,51 +4,97 @@ using System.Linq;
 using System.Text;
 using System.Data.Entity;
 using System.Data;
+using System.Data.Objects;
+using System.Data.Objects.DataClasses;
+using Persistence.Entity;
 
 namespace Persistence.DAO
 {
     public class Repositorio<T> : IRepositorio<T> where T : class
     {
-        protected readonly DbContext context;
+        protected readonly BDSysCadContext context;
+        protected string entitySetName;
 
-        public Repositorio(DbContext context)
+        protected string EntitySetName
         {
-            this.context = context;
+            get
+            {
+                if (String.IsNullOrEmpty(entitySetName))
+                    entitySetName = GetEntitySetName();
+
+                return entitySetName;
+            }
+        }
+
+        public Repositorio()
+        {
+            this.context = new BDSysCadContext();
         }
 
         public virtual void Adicionar(T item)
         {
-            context.Set<T>().Add(item);
-            context.SaveChanges();
+            context.AddObject(EntitySetName, item);
         }
 
         public virtual void Remover(T item)
         {
-            context.Set<T>().Remove(item);
-            context.SaveChanges();
+            context.DeleteObject(item);
         }
 
-        public virtual void Remover(object id)
-        {
-            T item = context.Set<T>().Find(id);
-            Remover(item);
-        }
-
+        /*
         public virtual void Atualizar(T item)
         {
-            context.Entry<T>(item).State = EntityState.Modified;
-            context.SaveChanges();
+            EntityKey key;
+            object originalItem;
+
+            if ((item.EntityKey == null)
+                // Obtém o entity key do objeto que será atualizado
+                key = context.CreateEntityKey(EntitySetName, item);
+            else
+                key = item.EntityKey;
+            try
+            {
+                // Obtém o objeto original
+                if (context.TryGetObjectByKey(key, out originalItem))
+                {
+                    if (originalItem is EntityObject &&
+                        ((EntityObject)originalItem).EntityState != EntityState.Added)
+                    {
+                        // Autaliza o objeto
+                        context.ApplyCurrentValues<T>(key.EntitySetName, item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }*/
+
+        /// Retorna um IQueryable com todos os objetos usando paginação
+        public IQueryable<T> QueryAll(int maximumRows, int startRowIndex)
+        {
+            return context.CreateQuery<T>(EntitySetName).Skip<T>(startRowIndex).Take(maximumRows);
         }
 
         public T FindOne(object id)
         {
-            return context.Set<T>().Find(id);
+            return context.CreateQuery<T>(EntitySetName, new ObjectParameter("key",id)).First<T>();
         }
 
-        public IQueryable FindAll()
+        public List<T> FindAll()
         {
-            return context.Set<T>();
+            return context.CreateQuery<T>(EntitySetName).ToList<T>();
         }
 
+        private string GetEntitySetName()
+        {
+            return String.Format("{0}Set",typeof(T).Name);
+        }
+
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+        }
     }
 }
---
+++ Persistence/Entity/Curso.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Text;
+using System.Data.Objects;
+using System.Data.EntityClient;
+
+
+namespace Persistence.Entity
+{
+    public class Curso
+    {
+        public Curso()
+        {
+            Modulos = new List<Modulo>();
+        }
+        public virtual int idCurso { get; set; }
+        public virtual ICollection<Modulo> Modulos { get; private set; }
+        public virtual string nome { get; set; }
+        public virtual string descricao { get; set; }
+        public virtual decimal valor { get; set; }
+        public virtual int status { get; set; }
+    }
+}
