a54038f8860580c8aa7d4884ecb8298d74a6ace1
leolirarj@gmail.com <leolirarj@gmail.com@d86ecb15-7846-d6a7-b0aa-8138a5d07417>
2012-04-15 17:05:49 +0000

    mapeamento das tabelas no context

--- trunk/Persistence/DAO/BDSysCadContext.cs
+++ trunk/Persistence/DAO/BDSysCadContext.cs
@@ -11,7 +11,21 @@ namespace Persistence.DAO
 {
     public class BDSysCadContext : ObjectContext
     {
+        private ObjectSet<Aluno> _aluno;
+        private ObjectSet<Cargo> _cargo;
+        private ObjectSet<Cobranca> _cobranca;
         private ObjectSet<Curso> _curso;
+        private ObjectSet<Endereco> _endereco;
+        private ObjectSet<Funcionario> _funcionario;
+        private ObjectSet<Matricula> _matricula;
+        private ObjectSet<MatriculaTurma> _matriculaTurma;
+        private ObjectSet<Modulo> _modulo;
+        private ObjectSet<NotaFalta> _notaFalta;
+        private ObjectSet<Perfil> _perfil;
+        private ObjectSet<Pessoa> _pessoa;
+        private ObjectSet<Taxa> _taxa;
+        private ObjectSet<TipoEndereco> _tipoEndereco;
+        private ObjectSet<Turma> _turma;
         private ObjectSet<Usuario> _usuario;
 
         public BDSysCadContext()
@@ -19,8 +33,47 @@ namespace Persistence.DAO
         {
             base.ContextOptions.ProxyCreationEnabled = true;
             base.ContextOptions.LazyLoadingEnabled = true;
+            _aluno = base.CreateObjectSet<Aluno>();
+            _cargo = base.CreateObjectSet<Cargo>();
+            _cobranca = base.CreateObjectSet<Cobranca>();
             _curso = base.CreateObjectSet<Curso>();
+            _endereco = base.CreateObjectSet<Endereco>();
+            _funcionario = base.CreateObjectSet<Funcionario>();
+            _matricula = base.CreateObjectSet<Matricula>();
+            _matriculaTurma = base.CreateObjectSet<MatriculaTurma>();
+            _modulo = base.CreateObjectSet<Modulo>();
+            _notaFalta = base.CreateObjectSet<NotaFalta>();
+            _perfil = base.CreateObjectSet<Perfil>();
+            _pessoa = base.CreateObjectSet<Pessoa>();
+            _taxa = base.CreateObjectSet<Taxa>();
+            _tipoEndereco = base.CreateObjectSet<TipoEndereco>();
+            _turma = base.CreateObjectSet<Turma>();
             _usuario = base.CreateObjectSet<Usuario>();
+            
+        }
+
+        public ObjectSet<Aluno> Aluno
+        {
+            get
+            {
+                return _aluno;
+            }
+        }
+
+        public ObjectSet<Cargo> Cargo
+        {
+            get
+            {
+                return _cargo;
+            }
+        }
+
+        public ObjectSet<Cobranca> Cobranca
+        {
+            get
+            {
+                return _cobranca;
+            }
         }
 
         public ObjectSet<Curso> Curso
@@ -31,13 +84,103 @@ namespace Persistence.DAO
             }
         }
 
-        public ObjectSet<Usuario> Curso
+        public ObjectSet<Endereco> Endereco
+        {
+            get
+            {
+                return _endereco;
+            }
+        }
+
+        public ObjectSet<Funcionario> Funcionario
+        {
+            get
+            {
+                return _funcionario;
+            }
+        }
+
+        public ObjectSet<Matricula> Matricula
+        {
+            get
+            {
+                return _matricula;
+            }
+        }
+
+        public ObjectSet<MatriculaTurma> MatriculaTurma
+        {
+            get
+            {
+                return _matriculaTurma;
+            }
+        }
+
+        public ObjectSet<Modulo> Modulo
+        {
+            get
+            {
+                return _modulo;
+            }
+        }
+
+        public ObjectSet<NotaFalta> NotaFalta
+        {
+            get
+            {
+                return _notaFalta;
+            }
+        }
+
+        public ObjectSet<Perfil> Perfil
+        {
+            get
+            {
+                return _perfil;
+            }
+        }
+
+        public ObjectSet<Pessoa> Pessoa
+        {
+            get
+            {
+                return _pessoa;
+            }
+        }
+
+        public ObjectSet<Taxa> Taxa
+        {
+            get
+            {
+                return _taxa;
+            }
+        }
+
+        public ObjectSet<TipoEndereco> TipoEndereco
+        {
+            get
+            {
+                return _tipoEndereco;
+            }
+        }
+
+        public ObjectSet<Turma> Turma
+        {
+            get
+            {
+                return _turma;
+            }
+        }
+
+
+        public ObjectSet<Usuario> Usuario
         {
             get
             {
                 return _usuario;
             }
         }
+
 
     }
 }
