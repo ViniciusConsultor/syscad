c2babdc8e5d045f22cc015dac2fcbf115dac9e39
diego.pessanha <diego.pessanha@grupoitec.com.br>
2012-04-15 04:22:32 +0000

    Criado todas as classes de Model, acerto de todos os nomes da tabela com Set e normal.

--- Persistence/Entity/Modulo.cs
+++ Persistence/Entity/Modulo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data;
+using System.Text;
 using System.Data.Objects;
 using System.Data.EntityClient;
 
@@ -10,11 +11,16 @@ namespace Persistence.Entity
 {
     public class Modulo
     {
+        public Modulo()
+        {
+            NotaFaltas = new List<NotaFalta>();
+        }
         public virtual int idModulo { get; set; }
         public virtual int idCurso { get; set; }
         public virtual string nome { get; set; }
         public virtual int tempoDuracao { get; set; }
         public virtual int statusModulo { get; set; }
-        public virtual Curso Curso { get; set; }
+        public virtual Curso Curso { get; set; } //um cara só
+        public virtual ICollection<NotaFalta> NotaFaltas { get; private set; }
     }
 }
