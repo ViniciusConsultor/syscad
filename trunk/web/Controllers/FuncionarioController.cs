d72b267abc9d09e99cff1d30e2aeaa3d7be5ef3f
diego.pessanha@grupoitec.com.br <diego.pessanha@grupoitec.com.br@d86ecb15-7846-d6a7-b0aa-8138a5d07417>
2012-04-24 01:40:53 +0000

    CRUD Funcionario Parte 2

--- trunk/web/Controllers/FuncionarioController.cs
+++ trunk/web/Controllers/FuncionarioController.cs
@@ -3,15 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Persistence.DAO;
+using Persistence.Entity;
+using System.Data;
+using Ext.Net;
+using System.Xml;
+using System.Xml.Xsl;
 
 namespace web.Controllers
 {
     public class FuncionarioController : Controller
     {
-
+        IRepositorio<Funcionario> dbFuncionario;
+        public FuncionarioController()
+        {
+            dbFuncionario = new Repositorio<Funcionario>();
+        }
+        
         public ActionResult Funcionario()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult FindAll()
+        {
+
+            IList<Funcionario> listaFuncionario = dbFuncionario.FindAll();
+            Funcionario funcionario = new Funcionario();
+            listaFuncionario.Add(funcionario);
+
+            return Json(new { funcionarios = listaFuncionario, totalReg = listaFuncionario.Count }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Excluir(int idFuncionario)
+        {
+
+            Funcionario funcionario = dbFuncionario.FindOne(cur => cur.idFuncionario == idFuncionario);
+
+            try
+            {
+                dbFuncionario.Remover(funcionario);
+                dbFuncionario.SaveChanges();
+
+                return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception e)
+            {
+
+                return Json(new { success = false, message = e.Message }, JsonRequestBehavior.AllowGet);
+
+            }
+
         }
 
     }
