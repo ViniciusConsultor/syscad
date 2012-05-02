836fc836cda52a332c4ff65c274cbbbd84efa57a
diego.pessanha@grupoitec.com.br <diego.pessanha@grupoitec.com.br@d86ecb15-7846-d6a7-b0aa-8138a5d07417>
2012-05-02 01:18:05 +0000

    CRUD Cargo e CRUD Pessoa

--- trunk/web/Controllers/PessoaController.cs
+++ trunk/web/Controllers/PessoaController.cs
@@ -19,6 +19,7 @@ namespace web.Controllers
         }
 
         IRepositorio<Pessoa> dbPessoa;
+
         public PessoaController()
         {
             dbPessoa = new Repositorio<Pessoa>();
@@ -32,11 +33,113 @@ namespace web.Controllers
 
         }
 
+        [HttpGet]
         public JsonResult FindAll()
         {
 
             IList<Pessoa> listaPessoa = dbPessoa.FindAll();
             return Json(new { pessoas = listaPessoa, totalReg = listaPessoa.Count }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public JsonResult Excluir(int id)
+        {
+
+            Pessoa pessoa = dbPessoa.FindOne(pes => pes.idPessoa == id);
+
+            try
+            {
+                dbPessoa.Remover(pessoa);
+                dbPessoa.SaveChanges();
+
+                return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception e)
+            {
+
+                return Json(new { success = false, message = e.Message }, JsonRequestBehavior.AllowGet);
+
+            }
+
+        }
+
+        [HttpPost]
+        public JsonResult Save(string txtNome, string txtCpf, string txtEmail, string txtTelefone, string txtCelular, string dtnascimento, int txtSexo)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.nome = txtNome;
+            pessoa.cpf = txtCpf;
+            pessoa.email = txtEmail;
+            pessoa.telefone = txtTelefone;
+            pessoa.celular = txtCelular;
+            pessoa.dataNascimento = Convert.ToDateTime(dtnascimento);
+            pessoa.sexo = Convert.ToInt32(txtSexo);
+
+            try
+            {
+
+                dbPessoa.Adicionar(pessoa);
+                dbPessoa.SaveChanges();
+
+                return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception e)
+            {
+
+                return Json(new { success = false, message = e.Message }, JsonRequestBehavior.AllowGet);
+
+            }
+
+        }
+
+        [HttpPost]
+        public JsonResult Editar(int id, string campo, string valor)
+        {
+
+            Pessoa pessoa = dbPessoa.FindOne(pes => pes.idPessoa == id);
+
+            try
+            {
+                switch (campo)
+                {
+                    case "nome":
+                        pessoa.nome = valor;
+                        break;
+                    case "cpf":
+                        pessoa.cpf = valor;
+                        break;
+                    case "email":
+                        pessoa.email = valor;
+                        break;
+                    case "telefone":
+                        pessoa.telefone = valor;
+                        break;
+                    case "celular":
+                        pessoa.celular = valor;
+                        break;
+                    case "dataNascimento":
+                        pessoa.dataNascimento = Convert.ToDateTime(valor);
+                        break;
+                    case "sexo":
+                        pessoa.sexo = Convert.ToInt32(valor);
+                        break;
+                }
+
+                dbPessoa.Atualizar(pessoa);
+                dbPessoa.SaveChanges();
+
+                return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception e)
+            {
+
+                return Json(new { success = false, message = e.Message }, JsonRequestBehavior.AllowGet);
+
+            }
 
         }
 
