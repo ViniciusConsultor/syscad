38093807fb75a02094afcc9e5819cd9930b79b0d
diego.pessanha <diego.pessanha@grupoitec.com.br>
2012-05-03 04:03:01 +0000

    Parte do CRUD de Pessoa e Endereco

---
+++ web/Controllers/EnderecoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Persistence.DAO;
+using Persistence.Entity;
+
+namespace web.Controllers
+{
+    public class EnderecoController : Controller
+    {
+
+        IRepositorio<Endereco> dbEndereco;
+        IRepositorio<Pessoa> dbPessoa;
+        IRepositorio<TipoEndereco> dbTipoEndereco;
+
+        public ActionResult Endereco()
+        {
+            return View();
+        }
+
+        public EnderecoController()
+        {
+            dbPessoa = new Repositorio<Pessoa>();
+            dbEndereco = new Repositorio<Endereco>();
+            dbTipoEndereco = new Repositorio<TipoEndereco>();
+
+        }
+
+        [HttpGet]
+        public JsonResult FindEnderecos(int idPessoa)
+        {
+            IList<Endereco> listaEndereco = dbEndereco.FindAll().Where(end => end.idPessoa == idPessoa).ToList();
+
+            foreach (Endereco e in listaEndereco)
+            {
+                e.Pessoa = dbPessoa.FindOne(pes => pes.idPessoa == e.idPessoa);
+                e.TipoEndereco = dbTipoEndereco.FindOne(te => te.idTipoEndereco == e.idTipoEndereco);
+            }
+
+            return Json(new { enderecos = listaEndereco, totalReg = listaEndereco.Count }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
