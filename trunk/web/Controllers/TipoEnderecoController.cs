bab48fe9c78c578a420a5cc0effcbac3a34b2b1d
diego.pessanha@grupoitec.com.br <diego.pessanha@grupoitec.com.br@d86ecb15-7846-d6a7-b0aa-8138a5d07417>
2012-05-05 20:10:09 +0000

    CRUD ENDERECO

---
+++ trunk/web/Controllers/TipoEnderecoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Persistence.DAO;
+using Persistence.Entity;
+
+namespace web.Controllers
+{
+    public class TipoEnderecoController : Controller
+    {
+        IRepositorio<TipoEndereco> dbTipoEndereco;
+
+        public TipoEnderecoController()
+        {
+            dbTipoEndereco = new Repositorio<TipoEndereco>();
+        }
+        
+        public ActionResult TipoEndereco()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Search(string limit, string query, string start)
+        {
+
+            IList<TipoEndereco> listaTipoEndereco = dbTipoEndereco.FindAll().Where(x => x.nome.ToLower().Contains(query.ToLower())).ToList();
+            return Json(new { tipoEnderecos = listaTipoEndereco, totalReg = listaTipoEndereco.Count }, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
