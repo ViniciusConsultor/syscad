6cfb9534d374a4d63c3ca7df6d82f93684d4a5aa
diego.pessanha <diego.pessanha@grupoitec.com.br>
2012-05-10 01:50:00 +0000

    Pessoa

--- web/Controllers/EnderecoController.cs
+++ web/Controllers/EnderecoController.cs
@@ -42,5 +42,38 @@ namespace web.Controllers
             return Json(new { enderecos = listaEndereco, totalReg = listaEndereco.Count }, JsonRequestBehavior.AllowGet);
         }
 
+        [HttpPost]
+        public JsonResult Save(int cmbTipoEndereco, int cmbPessoa, string txtLogradouro, int txtNumero, string txtComplemento, string txtCep, string txtBairro, string txtCidade, string txtUf)
+        {
+
+            Endereco endereco = new Endereco();
+            endereco.idTipoEndereco = cmbTipoEndereco;
+            endereco.idPessoa = cmbPessoa;
+            endereco.logradouro = txtLogradouro;
+            endereco.numero = txtNumero;
+            endereco.complemento = txtComplemento;
+            endereco.CEP = txtCep;
+            endereco.bairro = txtBairro;
+            endereco.cidade = txtCidade;
+            endereco.uf = txtUf;
+
+            try
+            {
+
+                dbEndereco.Adicionar(endereco);
+                dbEndereco.SaveChanges();
+
+                return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception e)
+            {
+
+                return Json(new { success = false, message = e.Message }, JsonRequestBehavior.AllowGet);
+
+            }
+
+        }
+
     }
 }
