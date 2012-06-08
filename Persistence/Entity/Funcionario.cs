dbf22390d6fe47816f9e83878ddbd8b5d588a1ce
diego.pessanha <diego.pessanha@grupoitec.com.br>
2012-06-08 03:32:47 +0000

    Acertos

--- Persistence/Entity/Funcionario.cs
+++ Persistence/Entity/Funcionario.cs
@@ -11,6 +11,10 @@ namespace Persistence.Entity
 {
     public class Funcionario
     {
+        public Funcionario()
+        {
+            Turmas = new List<Turma>();
+        }
         public virtual int idFuncionario { get; set; }
         public virtual int idUsuario { get; set; }
         public virtual int idCargo { get; set; }
@@ -19,6 +23,7 @@ namespace Persistence.Entity
         public virtual Cargo Cargo { get; set; }
         public virtual Pessoa Pessoa { get; set; }
         public virtual Usuario Usuario { get; set; }
+        public virtual ICollection<Turma> Turmas { get; set; }
         
     }
 }
